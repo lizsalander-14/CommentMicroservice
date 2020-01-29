49b3e0380d5ed6665226c99f07173f15175c28ec
swarnimgupta <guptaswarnim.14@gmail.com>
2020-01-29 10:10:29 +0530

    first commit

---
+++ src/test/java/com/project/commentMicroservice/CommentMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.commentMicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommentMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
